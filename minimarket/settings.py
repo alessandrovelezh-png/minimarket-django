e8cd2d0dd81887bd4580b759785b85b6c86b9422
alessandrovelezh-png <alessandro.velezh@gmail.com>
2026-07-06 18:14:33 -0500

    fix: Configuracion de zona horaria a America/Lima para validacion de Delivery

--- minimarket/settings.py
+++ minimarket/settings.py
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-pe'  # O 'es-mx', 'es' dependiendo de cómo lo tengas
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'  # <-- ESTE ES EL CAMBIO CLAVE
 
 USE_I18N = True
 
